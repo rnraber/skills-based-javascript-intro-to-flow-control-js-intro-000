77dd6af1259b7543e3771cb8e0800132f49c27d7
Ryann Raber <ryann.raber@yahoo.com>
2019-10-30 21:09:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,15 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!";
+    return "You are a teenager";
   }
 }
 
 function teenager(age) {
   if (age >=13 && age <= 19) {
-    console.log("You are a teenager!");
+    console.log("You are a teenager");
   }
   else {
-    return "You are not a teenager!";
+    return "You are not a teenager";
   }
 }
 
